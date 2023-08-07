b9c35539e4bb3e977c97d80d3c56c05be823554c
santisosaa <141378339+santisosaa@users.noreply.github.com>
2023-08-07 20:10:11 -0300

    scrip.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fdocument.querySelector("button.button-menu-toggle")
+document.querySelector("button.button-menu-toggle")
 .addEventListener("click",function() {
     document.querySelector(".nav-links").
     classList.toggle("nav-links-responsive")
