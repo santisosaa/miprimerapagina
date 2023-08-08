b8f42bab4d99847fea25329d9fca9efb5f8bb184
santisosaa <141378339+santisosaa@users.noreply.github.com>
2023-08-08 00:05:23 -0300

    mi primera confirmación de actualización

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 document.querySelector("button.button-menu-toggle")
 .addEventListener("click",function() {
     document.querySelector(".nav-links").
@@ -41,3 +42,50 @@ if (form) {
         lala.preventDefault()
         validateForm();
     })} 
+=======
+document.querySelector("button.button-menu-toggle")
+.addEventListener("click",function() {
+    document.querySelector(".nav-links").
+    classList.toggle("nav-links-responsive")
+}
+)
+
+
+function showAlert() { alert("¡Hola, esta es una alerta desde JavaScrip!") 
+}
+
+
+
+if (form) {
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    validateForm();
+})
+}
+
+const form= document.getElementById('myForm');
+
+function validateEmail(email) {
+const regex= /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+return regex.test(email)
+}
+
+function validateForm() {
+    const emailInput= document.getElementById('email');
+    const email= emailInput.value;
+
+    if (!validateEmail(email)) {
+        alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+        alert('Correo electrónico enviado correctamente.');
+    }
+}
+
+if (form) {
+    form.addEventListener('submit', function (lala){
+        lala.preventDefault()
+        validateForm();
+    }) 
+}
+
+>>>>>>> Stashed changes
