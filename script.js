6f684d64e20db80b4d23514e66b102135fbda577
santisosaa <141378339+santisosaa@users.noreply.github.com>
2023-08-07 21:46:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+document.querySelector("button.button-menu-toggle")
+.addEventListener("click",function() {
+    document.querySelector(".nav-links").
+    classList.toggle("nav-links-responsive")
+}
+)
+
+
+function showAlert() { alert("¡Hola, esta es una alerta desde JavaScrip!") 
+}
+
+
+
+if (form) {
+form.addEventListener('submit', function(event){
+    event.preventDefault();
+    validateForm();
+})
+}
+
+const form= document.getElementById('myForm');
+
+function validateEmail(email) {
+const regex= /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+return regex.test(email)
+}
+
+function validateForm() {
+    const emailInput= document.getElementById('email');
+    const email= emailInput.value;
+
+    if (!validateEmail(email)) {
+        alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+        alert('Correo electrónico enviado correctamente.');
+    }
+}
+
+if (form) {
+    form.addEventListener('submit', function (lala){
+        lala.preventDefault()
+        validateForm();
+    })} 
